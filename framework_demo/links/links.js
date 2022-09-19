06c432d571171f2385860cde73b99063d8636557
Sergei Pashkevich <65038584+iBombit@users.noreply.github.com>
2022-09-19 17:38:34 +0300

    Update links.js

--- framework_demo/links/links.js
+++ framework_demo/links/links.js
@@ -3,7 +3,7 @@ class DirectLinks {
 	get mainPage()  { return this.env};
 	get services()  { return "https://s.onliner.by/tasks"};
 	get baraholka() { return "https://baraholka.onliner.by/"};
-  get forum()     { return "https://forum.onliner.by/"};
+        get forum()     { return "https://forum.onliner.by/"};
 	get kurs()      { return "https://kurs.onliner.by/"};
 }
 
