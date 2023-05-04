0b86b1a6ff9ef56946f2a417556ff9a5d3fc694d
Sergei Pashkevich <65038584+iBombit@users.noreply.github.com>
2023-05-04 20:06:15 +0300

    fixed comment and powerapps class name

--- core/elements/iframe.js
+++ core/elements/iframe.js
@@ -3,30 +3,26 @@ const Element = require('./element');
 
 class Iframe extends Element {
     page;
-    
-    constructor(locator, page){
+
+    constructor(locator, page) {
         super(locator, page);
         this.page = page;
     }
 
-    /**
-     * Action: Create iframe with success status
-     * @selector CSS selector for iframe
-     * @scope    current scope (page in browser or other iframe)
-    */
-        async createIframe(timeout=Element.DEFAULT_TIMEOUT) {
-            logger.debug(`[IFRAME] ${this.locatorType}:${this.locator} in ${this.page.url()}`);
-            try {
-                let frameHandle = await this.find(timeout);
-                let frame = await frameHandle.contentFrame();
-                //each time we create a new frame we need to set status as sucess
-                //If it fails in "actions" -- it will skip other actions within this frame
-                frame.isSuccess = true;
-                return frame;
-            }
-            catch (error) {
-                logger.debug("[IFRAME] Can't find IFrame!")
-            }
+    //Action: Create iframe with success status
+    async createIframe(timeout = Element.DEFAULT_TIMEOUT) {
+        logger.debug(`[IFRAME] ${this.locatorType}:${this.locator} in ${this.page.url()}`);
+        try {
+            let frameHandle = await this.find(timeout);
+            let frame = await frameHandle.contentFrame();
+            //each time we create a new frame we need to set status as success
+            //If it fails in "actions" -- it will skip other actions within this frame
+            frame.isSuccess = true;
+            return frame;
+        }
+        catch (error) {
+            logger.debug("[IFRAME] Can't find IFrame!")
+        }
     }
 
 }
--- powerapps/pages.js
+++ powerapps/pages.js
@@ -4,7 +4,7 @@ const Button = require("../core/elements/button")
 const Element = require("../core/elements/element")
 const Iframe = require("../core/elements/iframe")
 
-class PowerAppsPage extends Page {
+class PowerApps extends Page {
     constructor(page) {
         super(page);
         this.url = "https://apps.powerapps.com/play/e/default-b41b72d0-4e9f-4c26-8a69-f949f367c91d/a/4f3abb6f-5e6a-4e4a-b3b2-09123d78a5f7?tenantId=b41b72d0-4e9f-4c26-8a69-f949f367c91d&source=portal/"
@@ -19,4 +19,4 @@ class PowerAppsPage extends Page {
 
 }
 
-module.exports = PowerAppsPage;
+module.exports = PowerApps;
